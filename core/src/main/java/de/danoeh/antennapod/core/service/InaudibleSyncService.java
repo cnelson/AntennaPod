563891ad942b753f197e5c004331d09191157032
Chris Nelson <cnelson@cnelson.org>
2019-10-17 14:10:41 -0700

    mvp of recurring opml import

--- core/src/main/java/de/danoeh/antennapod/core/service/InaudibleSyncService.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/InaudibleSyncService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.util.Pair;
 
@@ -13,6 +14,12 @@ import androidx.collection.ArrayMap;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.SafeJobIntentService;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -20,24 +27,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import de.danoeh.antennapod.core.ClientConfig;
 import de.danoeh.antennapod.core.R;
+import de.danoeh.antennapod.core.export.opml.OpmlElement;
+import de.danoeh.antennapod.core.export.opml.OpmlReader;
 import de.danoeh.antennapod.core.feed.Feed;
 import de.danoeh.antennapod.core.feed.FeedItem;
 import de.danoeh.antennapod.core.feed.FeedMedia;
 import de.danoeh.antennapod.core.preferences.InaudiblePreferences;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
+import de.danoeh.antennapod.core.service.download.AntennapodHttpClient;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.DBTasks;
 import de.danoeh.antennapod.core.storage.DBWriter;
 import de.danoeh.antennapod.core.storage.DownloadRequestException;
 import de.danoeh.antennapod.core.storage.DownloadRequester;
+import de.danoeh.antennapod.core.util.LangUtils;
 import de.danoeh.antennapod.core.util.NetworkUtils;
 import de.danoeh.antennapod.core.util.gui.NotificationUtils;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.Request;
 
 public class InaudibleSyncService extends SafeJobIntentService {
 
     private static final String TAG = "InaudibleSyncService";
 
-    private static final long WAIT_INTERVAL = 5000L;
+    private static final long WAIT_INTERVAL = 1000L;
 
     private static final String ARG_ACTION = "action";
 
@@ -57,7 +71,7 @@ public class InaudibleSyncService extends SafeJobIntentService {
         if (action != null) {
             switch(action) {
                 case ACTION_SYNC:
-                    Log.d(TAG, String.format("Waiting %d milliseconds before uploading changes", WAIT_INTERVAL));
+                    Log.d(TAG, String.format("Waiting %d milliseconds before syncing", WAIT_INTERVAL));
                     int syncActionId = syncActionCount.incrementAndGet();
                     try {
                         Thread.sleep(WAIT_INTERVAL);
@@ -86,6 +100,37 @@ public class InaudibleSyncService extends SafeJobIntentService {
         }
 
         Log.d(TAG, "Syncing from: " + InaudiblePreferences.getURL());
+        OkHttpClient client = AntennapodHttpClient.getHttpClient();
+        Response response = null;
+
+        try {
+            Request request = new Request.Builder()
+                    .url(InaudiblePreferences.getURL())
+                    .build();
+            response = client.newCall(request).execute();
+            OpmlReader opmlReader = new OpmlReader();
+            final List<String> localSubscriptions = DBReader.getFeedListDownloadUrls();
+
+            for (OpmlElement element : opmlReader.readDocument(response.body().charStream())) {
+                Log.d(TAG, "Got: "+element.getText());
+                if (!localSubscriptions.contains(element.getXmlUrl())) {
+                    Log.d(TAG, "Adding new feed: " + element.getXmlUrl());
+                    Feed feed = new Feed(element.getXmlUrl(), null,
+                            element.getText());
+                    DownloadRequester.getInstance().downloadFeed(this, feed);
+                }
+
+            }
+        } catch (Exception e) {
+            // TODO: quiet this once we see whats up
+            Log.d(TAG, Log.getStackTraceString(e));
+            updateErrorNotification(e);
+            return;
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
     }
 
     private void clearErrorNotifications() {
